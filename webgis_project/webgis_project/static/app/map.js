102b5c1513456e74f71d386db22fdf5d61a6ac9a
Amir Hossein <amirhossein183258@gmail.com>
2023-01-20 13:04:57 +0330

    6. Reverse geocode feature added.

--- webgis_project/webgis_project/static/app/map.js
+++ webgis_project/webgis_project/static/app/map.js
@@ -16,6 +16,7 @@ map.addLayer(osm);
 map.on("click", function (evt) {
     const coordinates = ol.proj.toLonLat(evt.coordinate);
     addMarker(coordinates);
+    revserseGeocode(coordinates);
 });
 
 var vectorSource = new ol.source.Vector({});
@@ -25,3 +26,21 @@ var dinamicPinLayer = new ol.layer.Vector({
 });
 
 map.addLayer(dinamicPinLayer);
+
+function revserseGeocode(coordiantes) {
+  request = "https://api.tomtom.com/search/2/reverseGeocode/" + String(coordiantes[1]) + "," + String(coordiantes[0]);
+  request += ".json?key=GmFrBSBi8VLDhqSbiGGT9KiugElcmPQQ&radius=100";
+
+  fetch(request)
+  .then((response) => response.json())
+  .then((data) => {
+    const addressBox = document.getElementById("address");
+
+    addressBox.style.display = "flex";
+    addressBox.style.alignItems= "center";
+    addressBox.style.justifyContent= "center";
+    addressBox.style.fontSize = "20px";
+    
+    addressBox.innerHTML = data.addresses[0].address.freeformAddress;
+  })
+}
