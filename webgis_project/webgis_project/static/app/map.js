d0dc16c2df5265fe8020911bc0a31b80d7e6e8fc
Amir Hossein <amirhossein183258@gmail.com>
2023-01-20 15:45:59 +0330

    7. Geoserver layers added to map.

--- webgis_project/webgis_project/static/app/map.js
+++ webgis_project/webgis_project/static/app/map.js
@@ -3,22 +3,53 @@ const map = new ol.Map({
 });
 
 const view = new ol.View({
-    center: ol.proj.fromLonLat([51.6, 32.6]),
-    zoom: 10
+    //center: ol.proj.fromLonLat([51.6, 32.6]),
+    center: [(-13884991 - 7455066) / 2, (6338219 + 2870341) / 2],
+    zoom: 3
 });
 map.setView(view);
 
+// OSM layer
 const osm = new ol.layer.Tile({
     source: new ol.source.OSM
 });
 map.addLayer(osm);
 
+// USA states layer
+const statesUSASource = new ol.source.TileWMS({
+    url: "http://localhost:8080/geoserver/wms",
+    params: {'LAYERS': 'topp:states', 'TILED': true},
+    serverType: 'geoserver',
+    transition: 0,
+});
+
+const statesUSA = new ol.layer.Tile({
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: statesUSASource
+});
+map.addLayer(statesUSA);
+
+// Iran states layer
+const iranStatesSource = new ol.source.TileWMS({
+    url: "http://localhost:8080/geoserver/wms",
+    params: {'LAYERS': 'nyc:States', 'TILED': true},
+    serverType: 'geoserver',
+    transition: 0,
+});
+
+const iranStates = new ol.layer.Tile({
+    source: iranStatesSource 
+});
+map.addLayer(iranStates);
+
+// Event handler
 map.on("click", function (evt) {
     const coordinates = ol.proj.toLonLat(evt.coordinate);
     addMarker(coordinates);
     revserseGeocode(coordinates);
 });
 
+// Marker definition
 var vectorSource = new ol.source.Vector({});
 
 var dinamicPinLayer = new ol.layer.Vector({
@@ -27,6 +58,7 @@ var dinamicPinLayer = new ol.layer.Vector({
 
 map.addLayer(dinamicPinLayer);
 
+// Reverse geocode function
 function revserseGeocode(coordiantes) {
   request = "https://api.tomtom.com/search/2/reverseGeocode/" + String(coordiantes[1]) + "," + String(coordiantes[0]);
   request += ".json?key=GmFrBSBi8VLDhqSbiGGT9KiugElcmPQQ&radius=100";
