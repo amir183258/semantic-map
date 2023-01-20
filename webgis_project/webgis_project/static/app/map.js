0df72e093d2afc2178bd811d1e8aa9e68ff4f368
Amir Hossein <amirhossein183258@gmail.com>
2023-01-21 03:07:46 +0330

    8. Identify featrue added for iran.

--- webgis_project/webgis_project/static/app/map.js
+++ webgis_project/webgis_project/static/app/map.js
@@ -60,19 +60,56 @@ map.addLayer(dinamicPinLayer);
 
 // Reverse geocode function
 function revserseGeocode(coordiantes) {
-  request = "https://api.tomtom.com/search/2/reverseGeocode/" + String(coordiantes[1]) + "," + String(coordiantes[0]);
-  request += ".json?key=GmFrBSBi8VLDhqSbiGGT9KiugElcmPQQ&radius=100";
+    request = "https://api.tomtom.com/search/2/reverseGeocode/" + String(coordiantes[1]) + "," + String(coordiantes[0]);
+    request += ".json?key=GmFrBSBi8VLDhqSbiGGT9KiugElcmPQQ&radius=100";
 
-  fetch(request)
-  .then((response) => response.json())
-  .then((data) => {
-    const addressBox = document.getElementById("address");
+    fetch(request)
+    .then((response) => response.json())
+    .then((data) => {
+        const addressBox = document.getElementById("address");
 
-    addressBox.style.display = "flex";
-    addressBox.style.alignItems= "center";
-    addressBox.style.justifyContent= "center";
-    addressBox.style.fontSize = "20px";
-    
-    addressBox.innerHTML = data.addresses[0].address.freeformAddress;
-  })
+        addressBox.style.display = "flex";
+        addressBox.style.alignItems= "center";
+        addressBox.style.justifyContent= "center";
+        addressBox.style.fontSize = "20px";
+        
+        addressBox.innerHTML = data.addresses[0].address.freeformAddress;
+  });
 }
+
+// Get feature information
+map.on("click", function (evt) {
+    var viewResolution = /** @type {number} */ (view.getResolution());
+    console.log(viewResolution);
+        var url = iranStatesSource.getGetFeatureInfoUrl(
+            evt.coordinate, viewResolution, 'EPSG:3857',
+            {'INFO_FORMAT': 'application/json'});
+    console.log(url);
+
+    result = document.getElementById("identify");
+
+    const headings = ["State", "Area", "Population"];
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.features.length > 0) {
+            const values = [data.features[0].properties.SOURCETHM,
+                            data.features[0].properties.AREA,
+                            data.features[0].properties.Population
+            ];
+
+            result.innerHTML = "<tr><th>" + headings[0] + "</th><th>" +
+                headings[1] + "</th><th>" + headings[2] + "</th></tr>" + 
+                "<tr><td>" + values[0] + "</td><td>" + values[1] + "</td><td>" + values[2] +
+                "</td>";
+        }
+        else {
+            result.style.border = "none";
+            result.innerHTML = "No Data";
+        }
+
+    });
+
+
+});
